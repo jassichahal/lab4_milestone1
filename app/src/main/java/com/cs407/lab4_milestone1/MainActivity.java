1356c13e5bb9df45b650f96510b15c8584c69517
Jassi Chahal <jassikkchahal@gmail.com>
2023-10-09 03:09:25 -0500

    commit and push - initially done with milestone 1

--- app/src/main/java/com/cs407/lab4_milestone1/MainActivity.java
+++ app/src/main/java/com/cs407/lab4_milestone1/MainActivity.java
@@ -2,15 +2,105 @@ package com.cs407.lab4_milestone1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Button startButton;
+    private volatile boolean stopThread = false;
+    private ProgressBar downloadProgressBar;
+    private TextView progressText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+        Button button2 = findViewById(R.id.button2);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch toggleSwitch = findViewById(R.id.toggleSwitch);
+        downloadProgressBar = findViewById(R.id.downloadProgressBar);
+        progressText = findViewById(R.id.progressText);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void mockFileDownloader() {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Downloading...");
+            }
+        });
+
+
+        for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress = downloadProgress + 10) {
+
+            if (stopThread) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startButton.setText("Start");
+                    }
+                });
+                return;
+            }
+
+            Log.d(TAG, "Download Progress: " + downloadProgress + "%");
+
+            //int finalDownloadProgress = downloadProgress;
+            int finalDownloadProgress = downloadProgress;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    downloadProgressBar.setProgress(finalDownloadProgress);
+                    progressText.setText("Download Progress: " + finalDownloadProgress + "%");
+                }
+            });
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Start");
+            }
+        });
+
+    }
+
+
+
+    public void startDownload(View view) {
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable();
+        new Thread(runnable).start();
+    }
+
+    public void stopDownload(View view) {
+        stopThread = true;
+    }
+
+    class ExampleRunnable implements Runnable {
+        @Override
+        public void run() {
+            mockFileDownloader();
+        }
     }
 }
+
+
 
 //commenting for initial commit!
